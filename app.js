8bcabb4bb4898975e06439340edd7527c08f2c46
MinjaeLee <sochubert@gmail.com>
2021-01-30 22:29:58 +0900

    signin backend

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var homeRouter = require('./routes/home');
+var signinRouter = require('./routes/index');
 
 var app = express();
 
